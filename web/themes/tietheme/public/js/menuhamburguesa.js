7c259da1f0002f433d97c63a2cd1ed8a6a597f75
Freddie Daza <info@clusterboss.com>
2021-07-27 19:35:27 -0500

    Actualizado Titheme 1.1.15

---
+++ web/themes/tietheme/public/js/menuhamburguesa.js
@@ -0,0 +1,6 @@
+/* OCULTA Y HACE VISIBLE MENU MOVIL CON BURGER ICON*/
+function toggleNavbar(collapseID) {
+    document.getElementById(collapseID).classList.toggle("hidden");
+    document.getElementById(collapseID).classList.toggle("block");
+  }
+  /* FIN OCULTA Y HACE VISIBLE MENU MOVIL CON BURGER ICON*/
